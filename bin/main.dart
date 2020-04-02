156c30674850e607c961688c0a26a13588e381fd
Ivanderson Bordin <ivandersonbordin@gmail.com>
2020-04-01 20:21:22 -0400

    teste de sistema

--- bin/main.dart
+++ bin/main.dart
@@ -4,12 +4,13 @@ import 'package:escola_teste/cpf.dart';
 
 void main() {
   final escola = Escola(
-    nome: 'Escolinha',
+    nome: 'Instituto Federal de RO',
     aluno: Aluno(
-      nome: 'Ivan',
-      cpf: Cpf('000.000.000-00'),
+      nome: 'Ivanderson',
+      cpf: Cpf('001.603.072-95'),
     ),
   );
 
   print(escola.aluno.cpf.verificado);
+  print('O nome da Escola é ${escola.nome} e possui um aluno com nome de: ${escola.aluno.nome} e de CPF nº ${escola.aluno.cpf.numero}');
 }
