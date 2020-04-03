3270f5358c3470df2714b37ec5cb2b82c046bb6b
Ivanderson Bordin <ivandersonbordin@gmail.com>
2020-04-02 22:13:50 -0400

    alterado descrição main

--- bin/main.dart
+++ bin/main.dart
@@ -8,14 +8,14 @@ void main() {
   var escola = Escola(
     aluno: Aluno(),
   );
-  escola.nome = readLine('Nome da Escola: ');
-  escola.aluno.nome = readLine('Nome da Aluno: ');
+  escola.nome = readLine('informe o nome da Escola: ');
+  escola.aluno.nome = readLine('Informe o nome da Aluno: ');
   escola.aluno.cpf = Cpf(
-    readLine('CPF do Aluno: '),
+    readLine('Informe o CPF do Aluno: '),
   );
 
   print(
-      'O nome da Escola é ${escola.nome} e possui um aluno com nome de: ${escola.aluno.nome} e de CPF nº ${escola.aluno.cpf.numero}');
+      'O nome da Escola é ${escola.nome} e possui um aluno com nome de: ${escola.aluno.nome} e CPF nº ${escola.aluno.cpf.numero}');
 }
 
 String readLine(String description) {
